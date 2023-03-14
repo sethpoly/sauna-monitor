ee48335697d46ad9202907cbe55b1d1c012fe6b1
Seth Polyniak <emailspolyniak@gmail.com>
2023-03-14 19:48:06 -0400

    [development] - rm uneeded routes

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,14 +3,14 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-background: #63D471;
+background: #FFFFFF;
 height: 85px;
 display: flex;
 justify-content: space-between;
 padding: 0.2rem calc((100vw - 1000px) / 2);
 z-index: 12;
-/* Third Nav */
-/* justify-content: flex-start; */
+@media screen and (max-width: 768px) {
+}
 `;
 
 export const NavLink = styled(Link)`
@@ -26,61 +26,12 @@ cursor: pointer;
 }
 `;
 
-export const Bars = styled(FaBars)`
-display: none;
-color: #808080;
-@media screen and (max-width: 768px) {
-	display: block;
-	position: absolute;
-	top: 0;
-	right: 0;
-	transform: translate(-100%, 75%);
-	font-size: 1.8rem;
-	cursor: pointer;
-}
-`;
-
 export const NavMenu = styled.div`
 display: flex;
 align-items: center;
 margin-right: -24px;
-/* Second Nav */
-/* margin-right: 24px; */
-/* Third Nav */
-/* width: 100vw;
-white-space: nowrap; */
 @media screen and (max-width: 768px) {
-	display: none;
+
 }
 `;
 
-export const NavBtn = styled.nav`
-display: flex;
-align-items: center;
-margin-right: 24px;
-/* Third Nav */
-/* justify-content: flex-end;
-width: 100vw; */
-@media screen and (max-width: 768px) {
-	display: none;
-}
-`;
-
-export const NavBtnLink = styled(Link)`
-border-radius: 4px;
-background: #808080;
-padding: 10px 22px;
-color: #000000;
-outline: none;
-border: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
-/* Second Nav */
-margin-left: 24px;
-&:hover {
-	transition: all 0.2s ease-in-out;
-	background: #fff;
-	color: #808080;
-}
-`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,33 +12,11 @@ const Navbar = () => {
 return (
 	<>
 	<Nav>
-		<Bars />
-
 		<NavMenu>
-		<NavLink to='/about' activeStyle>
-			About
+		<NavLink to='/' activeStyle>
+			RhodesSauna
 		</NavLink>
-		<NavLink to='/events' activeStyle>
-			Events
-		</NavLink>
-		<NavLink to='/annual' activeStyle>
-			Annual Report
-		</NavLink>
-		<NavLink to='/team' activeStyle>
-			Teams
-		</NavLink>
-		<NavLink to='/blogs' activeStyle>
-			Blogs
-		</NavLink>
-		<NavLink to='/sign-up' activeStyle>
-			Sign Up
-		</NavLink>
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
 		</NavMenu>
-		<NavBtn>
-		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
-		</NavBtn>
 	</Nav>
 	</>
 );
