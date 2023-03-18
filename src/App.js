3d795447d22cb40074362764d4b2e546c0beea43
Seth Polyniak <emailspolyniak@gmail.com>
2023-03-18 11:46:48 -0400

    [feat/sensor-layout] - style temp and timestamp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react'
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home/index';
 
 function App() {
 
--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,4 +1,12 @@
 export const formatTimestamp = (timestamp) => {
+    if(timestamp == undefined || timestamp == null || timestamp == '') {
+        return 'never';
+    }
     const date = new Date(timestamp * 1000);
     return date.toLocaleString();
+}
+
+export const formatTemp = (temp) => {
+    var value = Math.round(parseFloat(temp)) || '-';
+    return `${value}\u00B0F`;
 }
