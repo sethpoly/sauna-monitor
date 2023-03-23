f9dea073d51ef3aa6367fbed31459f4629b54681
Seth Polyniak <emailspolyniak@gmail.com>
2023-03-22 20:46:09 -0400

    [feat/book-time] - import date picker mui

--- src/components/Booking/index.js
+++ src/components/Booking/index.js
@@ -1,12 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import Button from '@mui/material/Button';
 import styles from './booking.module.css';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const Booking = () => {
+
+    const [showBookings, setShowBookings] = useState(false);
+    const onButtonClick = () => {
+        setShowBookings(true);
+    }
+
     return (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
         <div className={styles['container']} >
-            <Button onClick={() => { }} variant="contained" color="primary">Book Time</Button>
+            {showBookings ? null : <Button onClick={onButtonClick} variant="contained" color="primary">Book Time</Button>}
+            <div className={styles['book-time-container']}>
+                <h2 className={styles['header']}>Book Time</h2>
+                <DatePicker/>
+            </div>
         </div>
+        </LocalizationProvider>
     )
 }
 
