461de2ddb76188bdde06dc6a27a33fb0a0042f61
Seth Polyniak <emailspolyniak@gmail.com>
2023-03-25 08:44:35 -0400

    [feat/book-time] - add baseline material date components

--- src/components/Booking/index.js
+++ src/components/Booking/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { useState } from "react";
 import Button from '@mui/material/Button';
 import styles from './booking.module.css';
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { MobileDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const Booking = () => {
@@ -15,11 +16,20 @@ const Booking = () => {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
         <div className={styles['container']} >
-            {showBookings ? null : <Button onClick={onButtonClick} variant="contained" color="primary">Book Time</Button>}
-            <div className={styles['book-time-container']}>
+            {showBookings ? <div className={styles['book-time-container']}>
                 <h2 className={styles['header']}>Book Time</h2>
-                <DatePicker/>
-            </div>
+                <div className={styles['input-container']}>
+                    <h3>Select a day</h3>
+                    <MobileDatePicker disablePast="true"/>
+                </div>
+                <div className={styles['input-container']}>
+                    <h3>Select a time</h3>
+                    <MobileTimePicker disablePast="true"/>
+                </div>
+                <Button onClick={() => {}} variant="contained" color="primary">Confirm time</Button>
+            </div> : 
+            <Button onClick={onButtonClick} variant="contained" color="primary">Book Time</Button>}
+            
         </div>
         </LocalizationProvider>
     )
